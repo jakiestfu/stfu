b1e95497801b48d6b3dd010ae6a6f14ebd6cea81
Jacob Kelley <jakie8@gmail.com>
2015-04-07 14:23:39 -0700

    misc

--- nav/api.js
+++ nav/api.js
@@ -2,17 +2,18 @@
 
 	var init = function() {
 
-		var nav_script = doc.getElementById('jakiestfu-nav-api');
+		var body = doc.body;
 		var frame = doc.createElement('iframe');
 		frame.setAttribute('style', [
 			"display: block;",
 			"position: fixed;",
+			"width: 100%;",
 			"top: 0;",
 			"left:0;",
 			"right:0;",
-			"border:none;"
+			"border:none;",
 		].join(""));
-		nav_script.parentNode.insertBefore(frame, nav_script.nextSibling);
+		body.insertBefore(frame,body.firstChild);
 
 		frame.src = "//jakiestfu.github.io/lab-tools/nav/";
 	};
